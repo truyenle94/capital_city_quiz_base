166c743311e5f6b210fe140d0b053d7ae88578cc
truye <truyen91>
2019-02-28 16:30:41 -0600

    LAB 5.3

--- game.js
+++ game.js
@@ -3,18 +3,42 @@ let userAnswerElement = document.querySelector("#user-answer")
 let submitButton = document.querySelector("#submit-answer")
 let resultTextElement = document.querySelector('#result')
 
-
 // TODO when the page loads, select an element at random from the countriesAndCodes array
+let randomElement = countriesAndCodes[Math.floor(Math.random()*Object.keys(countriesAndCodes).length)]
 // This array is defined in the countries.js file. Your browser treats all 
 // JavaScript files as one big file, organized in the order of the script tags
 // so countriesAndCodes is available to this file 
 
-console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - just proving it is available 
+console.log(randomElement['alpha-2'])  // You don't need to log countriesAndCodes - just proving it is available
 
 // TODO display the country's name in the randomCountryElement 
-
+randomCountryElement.innerHTML=randomElement.name
 // TODO add a click event handler to the submitButton.  When the user clicks the button,
-//  * read the text from the userAnswerElement 
+submitButton.addEventListener('click',function () {
+    let userAnswer=userAnswerElement.value
+
+    var url = 'http://api.worldbank.org/v2/country/'+randomElement['alpha-2']+'?format=json'
+
+    fetch(url)
+        .then( res => res.json())
+        .then( issData => {
+            console.log(issData)
+
+            let capital = issData[1][0].capitalCity
+            console.log(capital)
+            if (userAnswer === capital){
+                resultTextElement.innerHTML=`Correct! The capital of `
+            }
+            else{
+                resultTextElement.innerHTML=`Wrong - the capital of ${randomElement.name} is not ${userAnswer}, it is ${capital}`
+            }
+        })
+        .catch( err => {
+            console.log(err)
+        })
+
+})
+//  * read the text from the userAnswerElement sdas
 //  * Use fetch() to make a call to the World Bank API with the country code (from countriesAndCodes)
 //  * Extract the capital city from the World Bank API response
 //  * Compare it to the user's answer. 
@@ -22,5 +46,4 @@ console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - jus
 //      the user's answer to be exactly the same as the World Bank answer. If you want 
 //      to be more flexible, include and use a string similarity library such as https://github.com/hiddentao/fast-levenshtein 
 //  * Display an appropriate result in the resultTextElement. 
-//      For example "Correct! The capital of Germany is Berlin" or "Wrong - the capital of Germany is not G, it is Berlin"
-
+//      For example "Correct! The capital of Germany is Berlin" or "Wrong - the capital of Germany is not G, it is Berlin"America & Caribbean "},"adminregion":{"id":"LAC","iso2code":"XJ","value":"Latin America & Caribbean (excluding high income)"},"incomeLevel":{"id":"UMC","iso2code":"XT","value":"Upper middle income"},"lendingType":{"id":"IBD","iso2code":"XF","value":"IBRD"},"capitalCity":"Belmopan","longitude":"-88.7713","latitude":"17.2534"}]]
